dc37364eb52f46eaa69450d6aa13786a7040c9f6
ayedrian <adrian.sohn@gmail.com>
2026-05-09 14:32:56 +0200

    Fix interactive behavior (handling of stdin) of Exec task for runWasm

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,4 +26,10 @@ tasks.register<Exec>("runWasm") {
         "-W", "function-references", // typed function references for lambdas, higher-order functions etc.
         "build/compileSync/wasmWasi/main/productionExecutable/optimized/kotlin-non-browser-wasm.wasm"
     )
+    // Gradle's JVM daemon has no real file descriptor 0, however it forwards stdin from the gradlew client into the
+    // Java System.in stream (in other words Gradle reroutes System.in away from the standard fd 0 to the socket from the client).
+    // Therefore connecting System.in to the Exec task's standardInput makes sure it gets sent to Wasmtime.
+    // ProcessBuilder.inheritIO() didn't work here, as it it inherits the daemon's empty file descriptor 0 (Wasmtime just sees EOF)
+    // --no-daemon also doesn't work as Gradle still spins up a JVM instance
+    standardInput = System.`in`
 }
